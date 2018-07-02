52eabb132dc0ccaa1fc07fd118b3738ac7df8634
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-07-01 20:05:16 -0600

    Adds first test

--- test/Guess_test.rb
+++ test/Guess_test.rb
@@ -1,8 +1,14 @@
+##this is very much a work in progress
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/guess'
 require 'pry'
 
 class GuessTest < Minitest::Test
-  #?
-  attr_accesor: card
+    attr_acce card
+  def test_it_exists
+    guess_1 = Guess.new("10 of hearts", card)
+    assert_instance_of Guess, guess_1
+  end
+
+end
